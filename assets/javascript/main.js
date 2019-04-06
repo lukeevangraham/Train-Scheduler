9051fd5a758f6806002414e2f8916e4146069e5a
Cefalu Wave <luke.evan.graham@gmail.com>
2019-04-05 19:06:15 -0700

    added firebase

---
+++ assets/javascript/main.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDXoWWWGL5Fd_1U0AJnbXeR2Ur0VoM54RY",
+    authDomain: "train-scheduler-fb382.firebaseapp.com",
+    databaseURL: "https://train-scheduler-fb382.firebaseio.com",
+    projectId: "train-scheduler-fb382",
+    storageBucket: "train-scheduler-fb382.appspot.com",
+    messagingSenderId: "181508120277"
+  };
+  firebase.initializeApp(config);
