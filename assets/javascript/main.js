c68085bb80ce1a4177ac0e83633d2c6cb6169a02
Cefalu Wave <luke.evan.graham@gmail.com>
2019-04-06 21:28:00 -0700

    working

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,11 +1,11 @@
 // Initialize Firebase
 var config = {
-apiKey: "AIzaSyDXoWWWGL5Fd_1U0AJnbXeR2Ur0VoM54RY",
-authDomain: "train-scheduler-fb382.firebaseapp.com",
-databaseURL: "https://train-scheduler-fb382.firebaseio.com",
-projectId: "train-scheduler-fb382",
-storageBucket: "train-scheduler-fb382.appspot.com",
-messagingSenderId: "181508120277"
+    apiKey: "AIzaSyDXoWWWGL5Fd_1U0AJnbXeR2Ur0VoM54RY",
+    authDomain: "train-scheduler-fb382.firebaseapp.com",
+    databaseURL: "https://train-scheduler-fb382.firebaseio.com",
+    projectId: "train-scheduler-fb382",
+    storageBucket: "train-scheduler-fb382.appspot.com",
+    messagingSenderId: "181508120277"
 };
 firebase.initializeApp(config);
 
@@ -19,7 +19,7 @@ var initialTime = 0;
 var frequency = "";
 
 // Capture Button Click
-$("#add-train").on("click", function(event) {
+$("#add-train").on("click", function (event) {
     // Don't refresh the page!
     event.preventDefault();
 
@@ -50,15 +50,15 @@ $("#add-train").on("click", function(event) {
 
     alert("Train successfully added");
 
-      // Clears all of the text-boxes
-  $("#name-input").val("");
-  $("#destination-input").val("");
-  $("#initial-time-input").val("");
-  $("#frequency-input").val("");
+    // Clears all of the text-boxes
+    $("#name-input").val("");
+    $("#destination-input").val("");
+    $("#initial-time-input").val("");
+    $("#frequency-input").val("");
 });
 
 // Firebase event for adding train to the database and a row in the html when the user adds an entry
-database.ref().on("child_added", function(childSnapshot){
+database.ref().on("child_added", function (childSnapshot) {
     console.log(childSnapshot.val());
 
     // store everything into a variable
@@ -76,29 +76,46 @@ database.ref().on("child_added", function(childSnapshot){
     var a = moment(trainInitTime, "hh:mma");
     var b = moment().format("hh:mma");
 
+    var timeFormat = "hh:mma"
+    var convertedTime = moment(trainInitTime, timeFormat);
+
+    // console.log("look here first", convertedTime.format("hh:mma"));
+
     // if train hasn't started
-    console.log(moment(a))
-    console.log(moment(b))
-    console.log(moment(b).isBefore(moment(a, 'minute')))
-    console.log("look here", moment('07:07am', "hh:mma").isBefore(moment('09:00am', "hh:mma")))
-
     if (moment(b, "hh:mma").isBefore(moment(a, "hh:mma"))) {
-        console.log(trainName, "train hasn't left yet")
-        nextArr = trainInitTime;
-    } 
+        // console.log(trainName, "train hasn't left yet")
+        nextArr = convertedTime.format("hh:mm A");
+        // console.log(nextArr)
+        // console.log("look at", moment(trainInitTime))
 
-    // Calculate the Minutes Away
-    a = moment(trainInitTime, "H:m");
-    b = moment().format("H:m");
-    var calc = moment(moment(b, "H:m").diff(moment(a, "H:m"))).format("mm") % trainFreq;
-    var minAway = trainFreq - calc
+        // Calculate the Minutes Away
+        // a = moment(trainInitTime, "hh:mma");
+        // b = moment().format("hh:mma");
+        // console.log(trainInitTime);
+        // console.log(b);
+        // var calc = moment(moment(b, "hh:mma").diff(moment(a, "hh:mma"))).format("mm");
+        // var minAway = trainFreq - calc
 
-    console.log(calc + " is calc")
+        var currentTime = moment().format(timeFormat);
+        // console.log("look here", currentTime);
+        // console.log("look here", convertedTime);
+        // console.log("look here", convertedTime.diff(currentTime));
 
-    // Calculate the next arrival
-    var duration = moment.duration({'minutes' : minAway})
-    var nextArrNoFormat = moment().add(duration);
-    var nextArr = moment(nextArrNoFormat).format("H:mm A");
+        console.log("look here friend", convertedTime.diff(moment(), "minutes"));
+        var minAway = convertedTime.diff(moment(), "minutes") + 1;
+
+    } else {
+        // Calculate the Minutes Away
+        a = moment(trainInitTime, "H:m");
+        b = moment().format("H:m");
+        var calc = moment(moment(b, "H:m").diff(moment(a, "H:m"))).format("mm") % trainFreq;
+        var minAway = trainFreq - calc
+
+        // Calculate the next arrival
+        var duration = moment.duration({ 'minutes': minAway })
+        var nextArrNoFormat = moment().add(duration);
+        var nextArr = moment(nextArrNoFormat).format("h:mm A");
+    }
 
     // Create the new row
     var newRow = $("<tr>").append(
