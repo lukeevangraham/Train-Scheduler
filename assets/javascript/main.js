e133a4a979e92393f2098d9dc80a88683da548e2
Cefalu Wave <luke.evan.graham@gmail.com>
2020-02-01 21:31:18 -0800

    trying without redirect

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -52,7 +52,7 @@ var uiConfig = {
   },
   // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
   signInFlow: 'popup',
-  signInSuccessUrl: '/Train-Scheduler/',
+  // signInSuccessUrl: '/Train-Scheduler/',
   signInOptions: [
     // Leave the lines as is for the providers you want to offer your users.
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
