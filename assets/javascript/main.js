c201f5058b0f6a71955d086a745fa0f654dda495
Cefalu Wave <luke.evan.graham@gmail.com>
2020-02-01 20:45:16 -0800

    better auth

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -9,7 +9,7 @@ var config = {
 };
 firebase.initializeApp(config);
 
-var provider = new firebase.auth.GoogleAuthProvider();
+// var provider = new firebase.auth.GoogleAuthProvider();
 
 // Create a variable to reference the database
 var database = firebase.database();
@@ -17,25 +17,37 @@ var database = firebase.database();
 let editClicked = false;
 var preventClearBtns = false;
 
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+ui.start('#firebaseui-auth-container', {
+  signInOptions: [
+    // List of OAuth providers supported.
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.GithubAuthProvider.PROVIDER_ID
+  ],
+  // Other config options...
+});
+
 // let currentData = []
 
 
-firebase.auth().signInWithPopup(provider).then(function(result) {
-  // This gives you a Google Access Token. You can use it to access the Google API.
-  var token = result.credential.accessToken;
-  // The signed-in user info.
-  var user = result.user;
-  // ...
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-});
+// firebase.auth().signInWithPopup(provider).then(function(result) {
+//   // This gives you a Google Access Token. You can use it to access the Google API.
+//   var token = result.credential.accessToken;
+//   // The signed-in user info.
+//   var user = result.user;
+//   // ...
+// }).catch(function(error) {
+//   // Handle Errors here.
+//   var errorCode = error.code;
+//   var errorMessage = error.message;
+//   // The email of the user's account used.
+//   var email = error.email;
+//   // The firebase.auth.AuthCredential type that was used.
+//   var credential = error.credential;
+//   // ...
+// });
 
 
 function clearForm() {
@@ -216,10 +228,10 @@ database.ref().on("child_changed", function(childSnapshot) {
   fillTable(childSnapshot, "replace")
 })
 
-$("#auth").click(function() {
-  console.log("authing")
-  firebase.auth().signInWithPopup(provider)
-})
+// $("#auth").click(function() {
+//   console.log("authing")
+//   firebase.auth().signInWithPopup(provider)
+// })
 
 // setInterval(prepFillTable, 60000);
 setInterval(adjustNextArrAndMinAway, 60000);
